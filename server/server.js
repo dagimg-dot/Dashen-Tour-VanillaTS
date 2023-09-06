c0158e829ffb89d135081ae56866dfa74194adf7
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-09-06 21:34:59 +0300

    formatting

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.get("/test", () => {
-    console.log('This is a test endpoint');
-})
+  console.log("This is a test endpoint");
+});
 
 app.listen(3000, () => {
-    console.log('Server listening on 3000');
-})
+  console.log("Server listening on 3000");
+});
