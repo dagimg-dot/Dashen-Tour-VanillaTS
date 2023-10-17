032341195d27a5e4e55ea86e2f3de49101490474
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-17 20:57:20 +0300

    add register endpoint

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const { default: registerUser } = require("./api/registerUser");
 
 const app = express();
 
 app.get("/test", () => {
   console.log("This is a test endpoint");
 });
+
+app.post("/api/register", registerUser);
 
 app.listen(3000, () => {
   console.log("Server listening on 3000");
