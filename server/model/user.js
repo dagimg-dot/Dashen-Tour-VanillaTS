b817c854f0753f6398f825b9ef3644ef0e8dad7f
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-17 20:56:08 +0300

    create user model

---
+++ server/model/user.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(fullName, email, phoneNumber, address, password) {
+    this.fullName = fullName;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.address = address;
+    this.password = password;
+  }
+}
+
+export default User;
