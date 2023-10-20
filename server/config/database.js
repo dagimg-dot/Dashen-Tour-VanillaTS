469c836caa3873b33d5117424eb85fa486b71993
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-20 22:41:03 +0300

    configure database using sequelize

---
+++ server/config/database.js
@@ -0,0 +1,13 @@
+const sequelize = require("sequelize");
+
+const connectDB = new sequelize({
+  dialect: "sqlite",
+  storage: "../db/dashen-tour.db",
+});
+
+connectDB
+  .sync()
+  .then(() => console.log("Database setup successfull!"))
+  .catch((error) => console.log("error: ", error));
+
+module.exports = connectDB;
