11aaf503b68cdff0cd53c60309718b22b60e9dbf
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-20 22:41:34 +0300

    create register endpoint

--- server/api/registerUser.js
+++ server/api/registerUser.js
@@ -1,33 +1,33 @@
-import { addUser } from "../dao/userDAO.js";
-import User from "../model/user";
+const User = require("../model/user");
 
-const registerUser = async (req, res) => {
+const registerUser = (req, res) => {
   const { fullName, email, phoneNumber, address, password } = req.body;
 
-  const newUser = new User(fullName, email, phoneNumber, address, password);
-
-  try {
-    const user = await addUser(newUser);
-
-    if (!user) {
+  User.create({
+    fullName,
+    email,
+    phoneNumber,
+    address,
+    password,
+  })
+    .then((user) => {
+      if (user) {
+        res.status(201).json({ message: "User registered successfully", user });
+      } else {
+        res.status(400).json({
+          error: {
+            message: "User registration failed",
+          },
+        });
+      }
+    })
+    .catch((err) => {
       res.status(400).json({
         error: {
-          message: "Registration failed",
+          message: "User registration failed",
         },
       });
-    }
-
-    res.status(200).json({
-      message: "Registration successful",
-      user,
     });
-  } catch (err) {
-    res.status(500).json({
-      error: {
-        message: `${err}`,
-      },
-    });
-  }
 };
 
-export default registerUser;
+module.exports = registerUser;
