dcce7e16bfa84dba19cb740146b23ddf290f451a
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-25 23:42:33 +0300

    create a route for the login endpoint

--- server/router/router.js
+++ server/router/router.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const login = require("../api/login")
 const registerUser = require("../api/registerUser")
 
 const router = express.Router()
 
 router.post("/api/register", registerUser)
+router.post("/api/login", login)
 
 module.exports = router
