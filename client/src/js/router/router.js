f51bd6fc9b825c3bf701aa6cc6720af6189505f6
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-27 01:13:28 +0300

    create router class to handle the entire web app routing

---
+++ client/src/js/router/router.js
@@ -0,0 +1,28 @@
+class Router {
+  constructor(coreElements, routes) {
+    this.root = coreElements.rootEl;
+    this.css = coreElements.css;
+    this.routes = routes;
+    this.currentRoute = null;
+  }
+
+  init() {
+    window.addEventListener("hashchange", this.handleRouteChange.bind(this));
+    this.handleRouteChange();
+  }
+
+  handleRouteChange() {
+    const hash = window.location.hash;
+
+    const route = this.routes.find((r) => r.path === hash.slice(1));
+
+    if (route) {
+      this.currentRoute = route;
+      route.controller(this.root, this.css);
+    } else {
+      this.root.textContent = "404 - Page not found";
+    }
+  }
+}
+
+export default Router;
