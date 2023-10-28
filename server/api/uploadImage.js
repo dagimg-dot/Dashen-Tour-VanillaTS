c4dce4d3552671ad3a7b205f9b945c2f0272ca9a
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-29 01:47:18 +0300

    setup multer for file upload

---
+++ server/api/uploadImage.js
@@ -0,0 +1,21 @@
+require("../config/env.config");
+
+const upload = (req, res) => {
+  if (!req.file) {
+    res.status(400).json({
+      error: {
+        message: "No image uploaded",
+      },
+    });
+  }
+
+  const link = `http://localhost:${process.env.PORT}/uploads/${req.file.filename}`;
+  res.status(200).json({
+    status: "200",
+    message: "Success",
+    data: { link },
+  });
+  console.log("sent");
+};
+
+module.exports = upload;
