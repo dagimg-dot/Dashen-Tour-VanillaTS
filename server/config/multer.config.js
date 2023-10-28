d80b599c960746128df239f3cd0fa559cf8152fa
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-29 02:09:15 +0300

    attach random byte to the file name

--- server/config/multer.config.js
+++ server/config/multer.config.js
@@ -1,16 +1,18 @@
 const multer = require("multer");
+const crypto = require("crypto");
 
 const upload = multer({
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB limit
+    fileSize: 2 * 1024 * 1024, // 5MB limit
   },
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "./uploads");
     },
     filename: function (req, file, cb) {
-        console.log('file:', file.originalname);
-      cb(null, Date.now().toString() + "-" + file.originalname);
+      const randomValue = crypto.randomBytes(6).toString("hex");
+      const ext = file.originalname.split(".").pop();
+      cb(null, Date.now().toString() + randomValue + "." + ext);
     },
   }),
 });
