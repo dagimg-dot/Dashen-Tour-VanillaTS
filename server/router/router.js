5755c5f0377f32f06661de9f55c4422314652b99
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-29 03:01:39 +0300

    implement file size handler method on upload endpoint

--- server/router/router.js
+++ server/router/router.js
@@ -3,11 +3,16 @@ const login = require("../api/login");
 const registerUser = require("../api/registerUser");
 const uploadImage = require("../api/uploadImage");
 const upload = require("../config/multer.config");
-
+const fileSizeLimitErrorHandler = require("../utils/fileSizeLimitErrorHandler");
 const router = express.Router();
 
 router.post("/api/register", registerUser);
 router.post("/api/login", login);
-router.post("/api/upload", upload.single("image"), uploadImage);
+router.post(
+  "/api/upload",
+  upload.single("image"),
+  fileSizeLimitErrorHandler,
+  uploadImage
+);
 
 module.exports = router;
