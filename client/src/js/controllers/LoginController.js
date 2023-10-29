431ef33278c1d95a2a4e688a8caa1c2252bfd562
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-30 01:51:15 +0300

    render login UI using the controller

--- client/src/js/controllers/LoginController.js
+++ client/src/js/controllers/LoginController.js
@@ -1,9 +1,11 @@
+import LoginView from "../views/LoginView.js";
+
 const LoginController = (root, css) => {
   // This is for parcel
   //   css.setAttribute("href", "/index.4cdbd4fe.css");
-  css.setAttribute("href", "./src/css/login.css");
 
-  root.innerText = "Login Page";
+  css.setAttribute("href", "./src/css/login.css");
+  root.innerHTML = LoginView.generateMarkup();
 };
 
 export default LoginController;
