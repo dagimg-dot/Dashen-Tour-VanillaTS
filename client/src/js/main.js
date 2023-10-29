5df37e58143fe790050a6adc303ed517521c64d6
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-30 02:41:08 +0300

    add title to core elements

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -2,10 +2,12 @@ import App from "./App.js";
 
 const root = document.getElementById("app");
 const dynamicCss = document.getElementById("dynamic-css");
+const dynamicTitle = document.getElementById("dynamic-title");
 
 const coreElements = {
-  rootEl: root,
+  root,
   css: dynamicCss,
+  title: dynamicTitle,
 };
 
 App(coreElements);
