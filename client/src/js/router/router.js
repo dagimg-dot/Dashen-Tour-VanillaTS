8b0ddae26008d347fc45561e148c6269bcdd901d
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-01 11:33:19 +0300

    add new methods getRoute and renderPageNotFound

--- client/src/js/router/router.js
+++ client/src/js/router/router.js
@@ -12,28 +12,36 @@ class Router {
     this.handleRouteChange();
   }
 
+  getRoute(route) {
+    return this.routes.find((r) => r.path === route);
+  }
+
+  renderPageNotFound() {
+    this.coreElements.root.innerHTML = PageNotFound.generateMarkup();
+  }
+
   push(path) {
-    const route = this.routes.find((r) => r.path === path);
+    const route = this.getRoute(path);
 
     if (route) {
       const newHash = "#" + path;
       this.currentRoute = route;
       window.location.hash = newHash;
     } else {
-      this.root.innerHTML = PageNotFound.generateMarkup();
+      this.renderPageNoteFound();
     }
   }
 
   handleRouteChange() {
     const hash = window.location.hash;
 
-    const route = this.routes.find((r) => r.path === hash.slice(1));
+    const route = this.getRoute(hash.slice(1));
 
     if (route) {
       this.currentRoute = route;
       route.controller(this.coreElements);
     } else {
-      this.root.innerHTML = PageNotFound.generateMarkup();
+      this.renderPageNotFound();
     }
   }
 }
