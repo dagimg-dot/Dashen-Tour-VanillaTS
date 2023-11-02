c778e1a1e86768707212e34523ecd7e6f8ecdf30
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-02 22:06:24 +0300

    hash and encrypt password when user is registered

--- server/api/registerUser.js
+++ server/api/registerUser.js
@@ -1,22 +1,28 @@
+const bcrypt = require("bcryptjs");
+
 const User = require("../model/user");
 const errorHandler = require("../utils/errorHandler");
 
 const registerUser = (req, res) => {
   const { fullName, email, phoneNumber, address, password } = req.body;
 
-  User.create({
-    fullName,
-    email,
-    phoneNumber,
-    address,
-    password,
-  })
-    .then((user) => {
-      res.status(201).json({ status: 201, message: "Success", data: { user } });
+  bcrypt.hash(password, 10).then(async (hash) => {
+    User.create({
+      fullName,
+      email,
+      phoneNumber,
+      address,
+      password: hash,
     })
-    .catch((error) => {
-      errorHandler(error, res);
-    });
+      .then((user) => {
+        res
+          .status(201)
+          .json({ status: 201, message: "Success", data: { user } });
+      })
+      .catch((error) => {
+        errorHandler(error, res);
+      });
+  });
 };
 
 module.exports = registerUser;
