5b69f1ebd28090c0c2c6fdba23ab1967ce99f3b2
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-04 01:50:24 +0300

    finish login controller with the reducers in use

--- client/src/js/controllers/LoginController.js
+++ client/src/js/controllers/LoginController.js
@@ -1,12 +1,58 @@
+import useReducer from "../hooks/useReducer.js";
+import reducer from "../reducers/LoginReducer.js";
 import LoginView from "../views/LoginView.js";
 
 const LoginController = ({ root, css, title }) => {
-  // This is for parcel
-  //   css.setAttribute("href", "/index.4cdbd4fe.css");
+  const initialState = {
+    rootNode: root,
+    credentials: {
+      email: "",
+      password: "",
+    },
+    isLoading: false,
+    isInvalid: false,
+  };
 
-  css.setAttribute("href", "./src/css/login.css");
-  title.innerText = "Dashen Tour - Login"
-  root.innerHTML = LoginView.render();
+  (() => {
+    // This is for live server
+    // css.setAttribute("href", "./src/css/login.css");
+
+    // This is for parcel
+    css.setAttribute("href", "/index.4cdbd4fe.css");
+    title.innerText = "Dashen Tour - Login";
+
+    // Initial Render
+    LoginView.render(initialState);
+  })();
+
+  const [state, dispatch, subscribe] = useReducer(reducer, initialState);
+
+  // Subscribe to state changes
+  subscribe(() => {
+    console.log("updating");
+    const newState = state();
+    console.log(newState);
+    // Update the view with the new state
+    LoginView.update(newState);
+  });
+
+  LoginView.handleSubmit(() => {
+    console.log("submitted");
+
+    if (state().isLoading) {
+      dispatch({ type: "SET_LOADING", payload: false });
+    } else {
+      const email = LoginView.getEmailValue();
+      const password = LoginView.getPasswordValue();
+
+      dispatch({ type: "SET_CREDENTIALS", payload: { email, password } });
+
+      dispatch({ type: "SET_LOADING", payload: true });
+      setTimeout(() => {
+        dispatch({ type: "SET_INVALID", payload: true });
+      }, 2000);
+    }
+  });
 };
 
 export default LoginController;
