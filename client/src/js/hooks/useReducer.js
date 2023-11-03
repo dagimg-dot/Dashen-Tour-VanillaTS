f2411c32490433db6103c6a753addb1af158a191
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-04 01:46:08 +0300

    create useReducer hook for state management

---
+++ client/src/js/hooks/useReducer.js
@@ -0,0 +1,25 @@
+const useReducer = (reducer, initialState) => {
+  let state = initialState;
+  const listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      const index = listeners.indexOf(listener);
+      if (index > -1) {
+        listeners.splice(index, 1);
+      }
+    };
+  };
+
+  return [getState, dispatch, subscribe];
+};
+
+export default useReducer;
