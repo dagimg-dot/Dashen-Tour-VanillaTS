b8e5678fb5e1590bc93a8bf60799b594a6df8303
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-03 17:47:26 +0300

    refactor: use reactive elements object to update Dom

--- client/src/js/utils/updateDOM.js
+++ client/src/js/utils/updateDOM.js
@@ -3,22 +3,33 @@ const createVirtualDOM = (content) => {
   return range.createContextualFragment(content);
 };
 
-const updateDOM = (stateFullElements, vDomStr) => {
+const updateDOM = (elements, vDomStr) => {
+  const { ReactiveElements } = elements;
   const vDOM = createVirtualDOM(vDomStr);
 
-  const refs = Object.keys(stateFullElements);
+  const refs = Object.keys(ReactiveElements);
 
   refs.forEach((ref) => {
     const vDomEl = vDOM.getElementById(ref);
-    const DomEl = stateFullElements[ref];
+    const DomEl = ReactiveElements[ref];
 
-    if (!vDomEl.isEqualNode(DomEl)) {
-      DomEl.innerHTML = vDomEl.innerHTML;
-    }
-    if (vDomEl.getAttribute("class") !== DomEl.getAttribute("class")) {
-      DomEl.setAttribute("class", vDomEl.getAttribute("class"));
+    if (Object.keys(ReactiveElements[ref]).includes("event")) {
+      // if (!vDomEl.isEqualNode(DomEl.el)) {
+      //   DomEl.el.innerHTML = vDomEl.innerHTML;
+      // }
+
+      if (vDomEl.getAttribute("class") !== DomEl.el.getAttribute("class")) {
+        DomEl.el.setAttribute("class", vDomEl.getAttribute("class"));
+      }
+      console.log(DomEl);
+    } else {
+      if (!vDomEl.isEqualNode(DomEl)) {
+        DomEl.innerHTML = vDomEl.innerHTML;
+      }
+      if (vDomEl.getAttribute("class") !== DomEl.getAttribute("class")) {
+        DomEl.setAttribute("class", vDomEl.getAttribute("class"));
+      }
     }
   });
 };
-
 export default updateDOM;
