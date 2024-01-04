cb47c20f618e0e95fec56ae535a9075818059ec5
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-04 19:52:09 +0300

    refactor: full type check

--- client/src/js/utils/updateDOM.ts
+++ client/src/js/utils/updateDOM.ts
@@ -1,42 +1,42 @@
+import { EventFullElement, HTMLTemplateLiteral } from "../types/types";
 import { isEventFull } from "./utilityFuncs";
 
-const createVirtualDOM = (content) => {
+const createVirtualDOM = (content: HTMLTemplateLiteral) => {
   const range = document.createRange();
-  return range.createContextualFragment(content);
+  return range.createContextualFragment(content as string);
 };
 
-const getElementFromDOM = (id) => {
+const getElementFromDOM = (id: string) => {
   return document.getElementById(id);
 };
 
-const updateDOM = (elements, vDomStr) => {
-  const { reactiveElements } = elements;
+const updateDOM = (reactiveElements, vDomStr: HTMLTemplateLiteral) => {
   const vDOM = createVirtualDOM(vDomStr);
 
   const refs = Object.keys(reactiveElements);
 
   refs.forEach((ref) => {
-    const vDomEl = vDOM.getElementById(ref);
-    let DomEl = reactiveElements[ref];
+    const vDomEl = vDOM.getElementById(ref) as HTMLElement;
 
-    if (isEventFull(DomEl)) {
-      DomEl.el = getElementFromDOM(ref);
+    if (isEventFull(reactiveElements[ref])) {
+      const DomEl: EventFullElement = reactiveElements[ref];
+      DomEl.el = getElementFromDOM(ref) as HTMLElement;
       // if (!vDomEl.isEqualNode(DomEl.el)) {
       //   DomEl.el.innerHTML = vDomEl.innerHTML;
       // }
 
       if (vDomEl.getAttribute("class") !== DomEl.el.getAttribute("class")) {
-        DomEl.el.setAttribute("class", vDomEl.getAttribute("class"));
+        DomEl.el.setAttribute("class", vDomEl.getAttribute("class") as string);
       }
       const event = `on${DomEl.event}`;
       DomEl.el[event] = DomEl.cb;
     } else {
-      DomEl = getElementFromDOM(ref);
+      const DomEl = getElementFromDOM(ref) as HTMLElement;
       if (!vDomEl.isEqualNode(DomEl)) {
         DomEl.innerHTML = vDomEl.innerHTML;
       }
       if (vDomEl.getAttribute("class") !== DomEl.getAttribute("class")) {
-        DomEl.setAttribute("class", vDomEl.getAttribute("class"));
+        DomEl.setAttribute("class", vDomEl.getAttribute("class") as string);
       }
     }
   });
