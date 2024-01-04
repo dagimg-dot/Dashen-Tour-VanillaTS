151031b5a92705f3191499602dcf72c14b135d58
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-04 16:15:06 +0300

    refactor: add types and remove .ts from import statements

--- client/src/js/App.ts
+++ client/src/js/App.ts
@@ -1,16 +1,18 @@
-import HomeController from "./controllers/HomeController.js";
-import SignUpController from "./controllers/SignUpController.js";
-import LoginController from "./controllers/LoginController.js";
-import useRouter from "./hooks/useRouter.js";
+import { Route, CoreElements } from "./types/types";
 
-const routes = [
+import HomeController from "./controllers/HomeController";
+import SignUpController from "./controllers/SignUpController";
+import LoginController from "./controllers/LoginController";
+import useRouter from "./hooks/useRouter";
+
+const routes: Route[] = [
   { path: "", controller: HomeController },
   { path: "/", controller: HomeController },
   { path: "/signup", controller: SignUpController },
   { path: "/login", controller: LoginController },
 ];
 
-export default function App(coreElements) {
+export default function App(coreElements: CoreElements) {
   const router = useRouter();
   router.init(coreElements, routes);
 }
