7c6340e3755f8d1d6fe2efc25a2be7297631a419
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-10 00:24:41 +0300

    feat: finish signup page fronend

--- client/src/ts/controllers/SignUpController.ts
+++ client/src/ts/controllers/SignUpController.ts
@@ -34,8 +34,65 @@ const SignUpController = ({ root, title }: CoreElements) => {
     SignupView.update(newState);
   });
 
+  const resetForm = (formContainer: HTMLFormElement) => {
+    dispatch([
+      { type: "SET_EMAIL", payload: "" },
+      { type: "SET_PASSWORD", payload: "" },
+      { type: "SET_FULLNAME", payload: "" },
+      { type: "SET_CONFIRM_PASSWORD", payload: "" },
+    ]);
+    formContainer.reset();
+  };
+
+  const checkError = (formContainer: HTMLFormElement): boolean => {
+    let errorFound = false;
+
+    if (state().fullNameErrorMessage !== "") {
+      const fullNameInput = formContainer.querySelector(
+        "#fullNameInput"
+      ) as HTMLInputElement;
+      fullNameInput.focus();
+      errorFound = true;
+      return errorFound;
+    }
+    if (state().emailErrorMessage !== "") {
+      const emailInput = formContainer.querySelector(
+        "#emailInput"
+      ) as HTMLInputElement;
+      emailInput.focus();
+      errorFound = true;
+      return errorFound;
+    }
+
+    if (state().passwordErrorMessage !== "") {
+      const paswordInput = formContainer.querySelector(
+        "#passwordInput"
+      ) as HTMLInputElement;
+      paswordInput.focus();
+      errorFound = true;
+      return errorFound;
+    }
+
+    if (state().confirmPasswordErrorMessage !== "") {
+      const confirmPaswordInput = formContainer.querySelector(
+        "#confirmPasswordInput"
+      ) as HTMLInputElement;
+      confirmPaswordInput.focus();
+      errorFound = true;
+      return errorFound;
+    }
+
+    return errorFound;
+  };
+
   SignupView.handleSubmit((event) => {
     event.preventDefault();
+
+    const formContainer = event.target as HTMLFormElement;
+
+    if (checkError(formContainer)) {
+      return;
+    }
 
     if (state().isLoading) {
       dispatch([{ type: "SET_LOADING", payload: false }]);
@@ -45,10 +102,78 @@ const SignUpController = ({ root, title }: CoreElements) => {
       // Imitating api request
       setTimeout(() => {
         dispatch([{ type: "SET_INVALID", payload: true }]);
-        // resetForm(formContainer);
+        resetForm(formContainer);
         console.log(state());
       }, 3000);
     }
+  });
+
+  const validateEmailField = (value: string) => {
+    const distpachEmailError = (error: string) => {
+      dispatch([{ type: "SET_EMAIL_ERROR", payload: error }]);
+    };
+
+    if (value == "") {
+      distpachEmailError("Email is required");
+    } else if (value.match("^[a-zA-Z0-9_]+@[a-zA-Z]+[.]+com$") === null) {
+      distpachEmailError("Invalid Email");
+    } else {
+      distpachEmailError("");
+    }
+  };
+
+  const validatePasswordField = (value: string) => {
+    const dispatchPasswordError = (error: string) => {
+      dispatch([{ type: "SET_PASSWORD_ERROR", payload: error }]);
+    };
+
+    if (value == "") {
+      dispatchPasswordError("Password is required");
+    } else if (value.length < 8) {
+      dispatchPasswordError("Password must be at least 8 characters");
+    } else {
+      dispatchPasswordError("");
+    }
+  };
+
+  const confirmPasswordField = (value: string) => {
+    const dispatchConfirmPasswordError = (error: string) => {
+      dispatch([{ type: "SET_CONFIRM_PASSWORD_ERROR", payload: error }]);
+    };
+
+    if (value == "" && state().password !== "") {
+      dispatchConfirmPasswordError("You need to confirm your password");
+    } else if (value !== state().password) {
+      dispatchConfirmPasswordError("The password you entered doesn't match");
+    } else {
+      dispatchConfirmPasswordError("");
+    }
+  };
+
+  SignupView.handleFullNameInput((event) => {
+    const target = event.target as HTMLInputElement;
+    dispatch([{ type: "SET_FULLNAME", payload: target.value }]);
+  });
+
+  SignupView.handleEmailInput((event) => {
+    const target = event.target as HTMLInputElement;
+    dispatch([{ type: "SET_EMAIL", payload: target.value }]);
+
+    validateEmailField(target.value);
+  });
+
+  SignupView.handlePasswordInput((event) => {
+    const target = event.target as HTMLInputElement;
+    dispatch([{ type: "SET_PASSWORD", payload: target.value }]);
+
+    validatePasswordField(target.value);
+  });
+
+  SignupView.handleConfirmPasswordInput((event) => {
+    const target = event.target as HTMLInputElement;
+    dispatch([{ type: "SET_CONFIRM_PASSWORD", payload: target.value }]);
+
+    confirmPasswordField(target.value);
   });
 };
 
