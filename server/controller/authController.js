b17b6ccb8f56eea54ecc71abc681ceb234b3e855
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-11 16:12:10 +0300

    fix: change login status codes to more general

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -5,6 +5,7 @@ import errorHandler from "../utils/errorHandler.js";
 
 const login = (req, res) => {
   const { email, password } = req.body;
+  console.log(email, password);
 
   if (!email || !password) {
     res.status(400).json({
@@ -16,20 +17,22 @@ const login = (req, res) => {
     try {
       User.findOne({ where: [{ email: email }] }).then((user) => {
         if (!user) {
-          res.status(404).json({
+          res.status(401).json({
             error: {
-              message: "Login failed",
+              message: "Incorrect email or password",
             },
           });
         } else {
           bcrypt.compare(password, user.password).then((result) => {
             result
-              ? res.status(202).json({
-                  status: 202,
+              ? res.status(200).json({
+                  status: 200,
                   message: "Success",
                   data: { user },
                 })
-              : res.status(400).json({ error: { message: "Login failed" } });
+              : res
+                  .status(401)
+                  .json({ error: { message: "Incorrect email or password" } });
           });
         }
       });
