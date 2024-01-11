05cebd0b0167ab1c8be9f5b93ce000971f81902a
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-11 21:54:25 +0300

    refactor: change promise handling to async/await

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 import User from "../model/user.js";
 import errorHandler from "../utils/errorHandler.js";
 
-const login = (req, res) => {
+const login = async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
@@ -14,67 +14,65 @@ const login = (req, res) => {
     });
   } else {
     try {
-      User.findOne({ where: [{ email: email }] }).then((user) => {
-        if (!user) {
-          res.status(401).json({
-            error: {
-              message: "Incorrect email or password",
-            },
-          });
-        } else {
-          bcrypt.compare(password, user.password).then((result) => {
-            result
-              ? res.status(200).json({
-                  status: 200,
-                  message: "Success",
-                  data: {
-                    fullName: user.fullName,
-                    email: user.email,
-                    phoneNumber: user.phoneNumber,
-                    address: user.address,
-                  },
-                })
-              : res
-                  .status(401)
-                  .json({ error: { message: "Incorrect email or password" } });
-          });
-        }
-      });
-    } catch (error) {
-      errorHandler(error);
-    }
-  }
-};
-
-const registerUser = (req, res) => {
-  const { fullName, email, phoneNumber, address, password } = req.body;
-
-  bcrypt.hash(password, 10).then(async (hash) => {
-    User.create({
-      fullName,
-      email,
-      phoneNumber,
-      address,
-      password: hash,
-    })
-      .then((user) => {
-        res.status(201).json({
-          status: 201,
-          message: "Success",
-          data: {
+      const user = await User.findOne({ where: [{ email: email }] });
+      if (!user) {
+        res.status(401).json({
+          error: {
+            message: "Incorrect email or password",
+          },
+        });
+      } else {
+        const result = await bcrypt.compare(password, user.password);
+        if (result) {
+          res.status(200).json({
+            status: 200,
+            message: "Success",
             data: {
               fullName: user.fullName,
               email: user.email,
               phoneNumber: user.phoneNumber,
               address: user.address,
             },
-          },
-        });
-      })
-      .catch((error) => {
-        errorHandler(error, res);
-      });
-  });
+          });
+        } else {
+          res
+            .status(401)
+            .json({ error: { message: "Incorrect email or password" } });
+        }
+      }
+    } catch (error) {
+      errorHandler(error);
+    }
+  }
+};
+
+const registerUser = async (req, res) => {
+  const { fullName, email, phoneNumber, address, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const user = await User.create({
+      fullName,
+      email,
+      phoneNumber,
+      address,
+      password: hashedPassword,
+    });
+    res.status(201).json({
+      status: 201,
+      message: "Success",
+      data: {
+        data: {
+          fullName: user.fullName,
+          email: user.email,
+          phoneNumber: user.phoneNumber,
+          address: user.address,
+        },
+      },
+    });
+  } catch (error) {
+    errorHandler(error, res);
+  }
 };
 
 export { login, registerUser };
