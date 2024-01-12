c14d82c553ddf4c5397a23a53830411345f9e220
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-12 23:09:36 +0300

    refactor: make properties of router class private and expose only needed ones

--- client/src/ts/router/router.ts
+++ client/src/ts/router/router.ts
@@ -3,47 +3,51 @@ import { CoreElements, Route, RoutePath } from "../types/types";
 import PageNotFound from "../views/PageNotFound";
 
 class Router {
-  coreElements!: CoreElements;
-  routes!: Route[];
-  currentRoute!: Route | null;
+  private coreElements!: CoreElements;
+  private routes!: Route[];
+  private currentRoute!: Route | null;
 
   init(coreElements: CoreElements, routes: Route[]) {
     this.coreElements = coreElements;
     this.routes = routes;
-    window.addEventListener("hashchange", this.handleRouteChange.bind(this));
-    this.handleRouteChange();
+    window.addEventListener("hashchange", this.#handleRouteChange.bind(this));
+    this.#handleRouteChange();
   }
 
-  getRoute(route: string) {
+  #getRoute(route: string) {
     return this.routes.find((r) => r.path === route);
   }
 
-  renderPageNotFound() {
+  #renderPageNotFound() {
     PageNotFound.renderView(this.coreElements.root);
   }
 
   push(path: RoutePath) {
-    const route = this.getRoute(path);
+    const route = this.#getRoute(path);
 
     if (route) {
       const newHash = "#" + path;
       this.currentRoute = route;
       window.location.hash = newHash;
     } else {
-      this.renderPageNotFound();
+      this.#renderPageNotFound();
     }
   }
 
-  handleRouteChange() {
+  getCurrentRoute() {
+    return this.currentRoute;
+  }
+
+  #handleRouteChange() {
     const hash = window.location.hash;
 
-    const route = this.getRoute(hash.slice(1));
+    const route = this.#getRoute(hash.slice(1));
 
     if (route) {
       this.currentRoute = route;
       route.controller(this.coreElements);
     } else {
-      this.renderPageNotFound();
+      this.#renderPageNotFound();
     }
   }
 }
