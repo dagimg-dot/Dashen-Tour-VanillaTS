90ccf6bf9dbc5d881ece988867924a7cb29fa345
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-13 02:15:41 +0300

    setup vite to proxy requests from frontend to backend server

---
+++ client/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
