4220a2085f5f779f41393e50189bcd376c6024d3
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-18 05:49:22 +0300

    create file for importing static assets

---
+++ client/src/static/index.ts
@@ -0,0 +1,12 @@
+//@ts-ignore
+import companyLogo from "../assets/icons/company-logo.svg";
+//@ts-ignore
+import facebook from "../assets/icons/facebook.svg";
+//@ts-ignore
+import instagram from "../assets/icons/instagram.svg";
+//@ts-ignore
+import twitter from "../assets/icons/twitter.svg";
+//@ts-ignore
+import youtube from "../assets/icons/youtube.svg";
+
+export { companyLogo, facebook, instagram, twitter, youtube };
