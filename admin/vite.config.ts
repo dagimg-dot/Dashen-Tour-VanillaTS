d931e279021eb97a94e9df7b9b1fcc29f7f20b07
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-23 00:17:59 +0300

    add proxy to the backend

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -3,4 +3,14 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+
 })
