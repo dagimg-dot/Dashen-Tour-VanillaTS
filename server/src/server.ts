75b262170bfc5edf2c197df9f9d301a9b5275745
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-24 00:33:29 +0300

    include destination routes in main app instantiation

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,10 @@ import { AuthRoute } from '@routes/auth.route';
 import { ValidateEnv } from '@utils/validateEnv';
 import { UserRoute } from '@routes/users.route';
 import { UploadRoute } from '@/routes/upload.route';
+import { DestinationRoute } from './routes/destinations.route';
 
 ValidateEnv();
 
-const app = new App([new AuthRoute(), new UserRoute(), new UploadRoute()]);
+const app = new App([new AuthRoute(), new UserRoute(), new UploadRoute(), new DestinationRoute()]);
 
 app.listen();
