808020e410e4a2b83c4959c701076e077bfec7e5
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-25 00:05:26 +0300

    feat: add layout for dashboard page

--- admin/src/pages/Dashboard.tsx
+++ admin/src/pages/Dashboard.tsx
@@ -1,5 +1,12 @@
+import SideNavigation from "../components/SideNavigation";
+
 const Dashboard = () => {
-  return <div class="text-4xl">Dashboard</div>;
+  return (
+    <div class="bg-main min-h-screen flex">
+      <SideNavigation />
+      <div class="bg-white flex-grow m-2 ml-0 rounded-lg">Dashboard</div>            
+    </div>
+  );
 };
 
 export default Dashboard;
