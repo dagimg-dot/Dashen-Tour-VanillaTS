e4107f2de6586bec224a85aec86f37a35df34a51
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-25 00:03:20 +0300

    feat: add custom color for consistent design system

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#411d0d",
+      }
+    },
   },
   plugins: [],
 }
