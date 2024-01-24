0684f1b575ab347a18d7f6813a69e0c7996e8e20
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-24 20:58:09 +0300

    wite test for destinations routoes

--- server/src/test/destinations.test.ts
+++ server/src/test/destinations.test.ts
@@ -35,6 +35,49 @@ describe('Testing Destinations', () => {
     });
   });
 
+  describe('[GET] /destinations/:id', () => {
+    it('response fineOne destination', async () => {
+      const destinationId = 1;
+
+      const destinationsRoute = new DestinationRoute();
+      const destinations = destinationsRoute.destinationsController.destinationService.destinations;
+
+      destinations.findByPk = jest.fn().mockReturnValue({
+        id: 1,
+        name: 'Bahir Dar',
+        description: 'this is a description',
+      });
+
+      (Sequelize as any).authenticate = jest.fn();
+      const app = new App([destinationsRoute]);
+      return request(app.getServer()).get(`${destinationsRoute.path}/${destinationId}`).expect(200);
+    });
+  });
+
+  describe('[PUT] /destinations/:id', () => {
+    it('response Update destination', async () => {
+      const destinationId = 1;
+      const destinationData: Destination = {
+        name: 'Addis Ababa',
+        description: 'hello from desc',
+      };
+
+      const destinationsRoute = new DestinationRoute();
+      const destinations = destinationsRoute.destinationsController.destinationService.destinations;
+
+      destinations.update = jest.fn().mockReturnValue([1]);
+      destinations.findByPk = jest.fn().mockReturnValue({
+        id: destinationId,
+        name: 'Addis Ababa',
+        description: 'hello from desc',
+      });
+
+      (Sequelize as any).authenticate = jest.fn();
+      const app = new App([destinationsRoute]);
+      return request(app.getServer()).put(`${destinationsRoute.path}/${destinationId}`).send(destinationData).expect(200);
+    });
+  });
+
   describe('[POST] /destinations', () => {
     it('response Create Destination', async () => {
       const destinationData: Destination = {
@@ -53,6 +96,25 @@ describe('Testing Destinations', () => {
       (Sequelize as any).authenticate = jest.fn();
       const app = new App([destinationsRoute]);
       return request(app.getServer()).post(`${destinationsRoute.path}`).send(destinationData).expect(201);
+    });
+  });
+
+  describe('[DELETE] /destination/:id', () => {
+    it('response delete destinations', async () => {
+      const destinationId = 1;
+
+      const destinationsRoute = new DestinationRoute();
+      const destinations = destinationsRoute.destinationsController.destinationService.destinations;
+
+      destinations.findByPk = jest.fn().mockReturnValue({
+        id: 1,
+        name: 'Bahir Dar',
+        description: 'this is a description',
+      });
+
+      (Sequelize as any).authenticate = jest.fn();
+      const app = new App([destinationsRoute]);
+      return request(app.getServer()).get(`${destinationsRoute.path}/${destinationId}`).expect(200);
     });
   });
 });
