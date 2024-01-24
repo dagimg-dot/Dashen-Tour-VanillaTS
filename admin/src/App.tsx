5710072e595ec3a5ffa12cf95cea507be08d4163
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-25 00:03:41 +0300

    add destinations page

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Router } from "@solidjs/router";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import Destinations from "./pages/Destinations";
 
 function App() {
   return (
     <Router>
       <Route path={"/"} component={Dashboard} />
+      <Route path={"/destinations"} component={Destinations} />
       <Route path={"/login"} component={Login} />
     </Router>
   );
---
+++ admin/src/pages/Destinations.tsx
@@ -0,0 +1,12 @@
+import SideNavigation from "../components/SideNavigation";
+
+const Destinations = () => {
+  return (
+    <div class="bg-main min-h-screen flex">
+      <SideNavigation />
+      <div class="bg-white flex-grow m-2 ml-0 rounded-lg">Destinations</div>            
+    </div>
+  );
+};
+
+export default Destinations;
