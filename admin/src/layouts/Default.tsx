d4b577a25aa3af73e008195dbc39e83f377ec7f6
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-29 23:46:25 +0300

    fix: customize scrolbar

--- admin/src/layouts/Default.tsx
+++ admin/src/layouts/Default.tsx
@@ -8,9 +8,9 @@ type DefaultLayoutProps = {
 const Default = ({ children }: DefaultLayoutProps) => {
   return (
     <div>
-      <div class="bg-main min-h-screen flex">
+      <div class="bg-main h-screen flex">
         <SideNavigation />
-        <div class="bg-white flex-grow m-2 ml-0 rounded-lg p-5">{children}</div>
+        <div class="bg-white flex-grow m-2 ml-0 rounded-lg p-5 overflow-y-auto">{children}</div>
       </div>
     </div>
   );
