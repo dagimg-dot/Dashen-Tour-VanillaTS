13a6ff939147b9ad80ee7d9e2add6d0b84a08df4
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-02-07 23:13:02 +0300

    feat: add second spinner

--- admin/src/components/Spinner.tsx
+++ admin/src/components/Spinner.tsx
@@ -1,9 +1,33 @@
-import "../css/spinner.css"
+import "../css/spinner.css";
 
-const Spinner = () => {
-  return (
-    <div class="loader"></div>
-  )
+interface SpinnerProps {
+  message?: string;
 }
 
-export default Spinner
+const SpinnerOne = () => {
+  return <div class="loader"></div>;
+};
+
+const SpinnerTwo = (props: SpinnerProps) => {
+  return (
+    <div>
+      <div class="lds-default absolute left-1/2 top-14 -translate-x-1/2">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="text-center mt-14">{props.message}</div>
+    </div>
+  );
+};
+
+export { SpinnerOne, SpinnerTwo };
