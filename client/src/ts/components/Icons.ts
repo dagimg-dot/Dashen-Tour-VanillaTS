5b656380d11090952a43e58927c5e337d851a386
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-02-20 21:56:43 +0300

    feat: add the star icon for rating

--- client/src/ts/components/Icons.ts
+++ client/src/ts/components/Icons.ts
@@ -73,4 +73,14 @@ const CompanyLogoIcon = html`
   <h1 class="nav-company-name">Dashen Tour</h1>
 `;
 
-export { SuccessIcon, ErrorIcon, SearchIcon, CompanyLogoIcon };
+const StarIcon = html`
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="35">
+    <path
+      d="M480 208H308L256 48l-52 160H32l140 96-54 160 138-100 138 100-54-160z"
+      stroke-linejoin="round"
+      fill="white"
+    />
+  </svg>
+`;
+
+export { SuccessIcon, ErrorIcon, SearchIcon, CompanyLogoIcon, StarIcon };
