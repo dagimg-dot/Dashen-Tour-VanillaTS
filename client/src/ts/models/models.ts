399a116ef6a9d0a6b7640ad0125336b60c874c6f
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-03-03 11:21:04 +0300

    feat: create package type for type safety

--- client/src/ts/models/models.ts
+++ client/src/ts/models/models.ts
@@ -6,3 +6,12 @@ export type Destination = {
   rating: number | null;
   images: { url: string }[];
 };
+
+export interface Package {
+  packageId?: number;
+  name: string;
+  numberOfDays: number;
+  hotelStar: number;
+  numberOfLocations: number;
+  isFeatured: boolean;
+}
