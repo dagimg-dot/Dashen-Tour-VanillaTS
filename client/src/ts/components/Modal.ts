67bb25ea448bcf6c33501617978ee51bc730359a
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-03-25 00:16:13 +0300

    feat: apply shimmer effect when loading images

--- client/src/ts/components/Modal.ts
+++ client/src/ts/components/Modal.ts
@@ -3,9 +3,11 @@ import "../../css/components/modal.css";
 import { CloseIcon } from "./Icons";
 import { Destination } from "../models/models";
 import { fetchDestination } from "../api/destination.api";
+import { iterator } from "../utils/utilityFuncs";
 
 interface ModalProps {
-  destinationInfo: Destination;
+  destinationInfo?: Destination;
+  isLoading?: boolean;
 }
 
 const skeletonObj = {
@@ -16,45 +18,53 @@ const skeletonObj = {
   images: [{ url: "" }],
 };
 
-const Modal = ({ destinationInfo }: ModalProps) => {
+const Modal = ({ destinationInfo = skeletonObj, isLoading }: ModalProps) => {
+  const modalCont = document.getElementById("modal") as HTMLDivElement;
+
   const template = html`<div class="dest-overlay">
     <div class="dest-modal">
       <div class="dest-modal-header">
         <div></div>
-        <div class="dest-modal-heading">
-          ${destinationInfo.name + " | " + destinationInfo.location}
-        </div>
+        ${isLoading
+          ? html`<div
+              class="dest-modal-heading skeleton-text"
+              style="width: 23rem; height: 3rem"
+            ></div>`
+          : html`<div class="dest-modal-heading">
+              ${destinationInfo.name + " | " + destinationInfo.location}
+            </div>`}
         <div class="close-container" id="closeBtn">${CloseIcon}</div>
       </div>
       <div class="dest-modal-imgs">
-        ${destinationInfo.images.map((img) => {
-          return html`<div class="dest-modal-img">
-            <img crossorigin="anonymous" src=${img.url} />
-          </div>`;
-        })}
+        ${isLoading
+          ? iterator(6).map(() => {
+              return html`<div
+                class="dest-modal-img skeleton"
+                style="width: 45rem; height: 35rem"
+              ></div>`;
+            })
+          : destinationInfo.images.map((img) => {
+              return html`<div class="dest-modal-img">
+                <img crossorigin="anonymous" src=${img.url} />
+              </div>`;
+            })}
       </div>
     </div>
   </div>`;
 
-  return template;
+  render(template, modalCont);
 };
 
-const useState = <S extends Destination>(container: HTMLElement) => {
+const useState = <S>(Render: Function) => {
   function setState(newState: S) {
-    render(Modal({ destinationInfo: newState }), container);
+    Render({ destinationInfo: newState, isLoading: false });
   }
 
   return setState;
 };
 
 const ModalController = async (destinationId: number) => {
-  const modalCont = document.getElementById("modal") as HTMLDivElement;
-  render(Modal({ destinationInfo: skeletonObj }), modalCont);
-
-  const setDestination = useState<Destination>(modalCont);
-
-  const destinationInfo = await fetchDestination(destinationId);
-  setDestination(destinationInfo);
+  Modal({ isLoading: true });
 
   const modal = document.querySelector(".dest-modal") as HTMLDivElement;
   const destOverlay = document.querySelector(".dest-overlay") as HTMLDivElement;
@@ -77,6 +87,11 @@ const ModalController = async (destinationId: number) => {
   };
 
   open();
+
+  const setDestination = useState<Destination>(Modal);
+
+  const destinationInfo = await fetchDestination(destinationId);
+  setDestination(destinationInfo);
 
   destOverlay.onclick = (event: MouseEvent) => {
     if ((event.target as HTMLDivElement).contains(modal)) {
